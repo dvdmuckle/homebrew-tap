9a891161f2c7f374b1269a004178f9f5d2db2900
David Muckle <dvdmuckle@dvdmuckle.xyz>
2020-08-20 00:25:43 -0400

    Revert "Add shell completion for spc"
    This reverts commit c8449934e6507209b8aba609b7e7f36ae0dac9a2.

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -11,16 +11,10 @@ class Spc < Formula
   elsif OS.linux?
     url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc_#{version}_linux_amd64.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  end
+end
 
   def install
     bin.install "spc"
-
-    output = Utils.safe_popen_read({ "SHELL" => "bash" }, bin/"spc", "completion", "bash")
-    (bash_completion/"spc").write output
-
-    output = Utils.safe_popen_read({ "SHELL" => "zsh" }, bin/"spc", "completion", "zsh")
-    (zsh_completion/"_spc").write output
-  end
+end
 
 end
