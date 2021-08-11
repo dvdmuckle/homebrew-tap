69aad914e7e76db9cf3670d3ebecc632cbe9dad6
David Muckle <dvdmuckle@dvdmuckle.xyz>
2021-08-10 20:45:18 -0400

    Update spc

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -1,5 +1,5 @@
 class Spc < Formula
-  version "0.6.2"
+  version "0.7.0"
   desc "A spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
 
@@ -7,7 +7,7 @@ class Spc < Formula
 
   if OS.mac?
     url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
-    sha256 "7ea975d604ffe7ac17ae87e19c9fc163ab73aa4f924445295bf687373ea391a4"
+    sha256 "98aa7615d5a64efe943168385628fc307331dc4d6dd36a12975e283202949b46"
   elsif OS.linux?
     url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
