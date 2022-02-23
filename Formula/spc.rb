f541144bcb49b0e8c3b1647659c577781ee04b03
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-02-22 20:19:19 -0500

    Support M1 installs

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -3,13 +3,24 @@ class Spc < Formula
   desc "A spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
 
-  if OS.mac?
-    url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
-    sha256 "58ae3964ca15acb67162562ca65c8b86f06344457bcaeb076cbe27ae565851a6"
-  elsif OS.linux?
-    url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
-    sha256 "427b08adb22ca8fb46a54d7df599e1f558a5f4e6c9493a2bfb75a5a091e672f2"
-end
+  if Hardware::CPU.intel?
+    if OS.mac?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
+      sha256 "58ae3964ca15acb67162562ca65c8b86f06344457bcaeb076cbe27ae565851a6"
+    elsif OS.linux?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
+      sha256 "427b08adb22ca8fb46a54d7df599e1f558a5f4e6c9493a2bfb75a5a091e672f2"
+    end
+  end
+  if Hardware::CPU.arm?
+    if OS.mac?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-arm64.tar.gz"
+      sha256 "f2e69b06073de82b2b55d9ee4206b544a4ed299d6a532593f9af34ea3ef563c4"
+    elsif OS.linux?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-arm64.tar.gz"
+      sha256 "c3ea843d3ca5a55f710ecd3aec4670053545b295c9cb5585456c51810ddb423b"
+    end
+  end
 
   def install
     bin.install "spc"
--- scripts/spc.sh
+++ scripts/spc.sh
@@ -15,21 +15,30 @@ CURL=`which curl`
 [ -n ${SHASUM} ] && [ -n ${CURL} ] || exit 2
 
 # Check if macOS release exists, if it does then it is likely that the Linux one does too
-MAC_TAR_URL="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-darwin-amd64.tar.gz"
-LINUX_TAR_URL="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-linux-amd64.tar.gz"
+MAC_TAR_URL_X64="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-darwin-amd64.tar.gz"
+LINUX_TAR_URL_X64="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-linux-amd64.tar.gz"
+MAC_TAR_URL_ARM="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-darwin-arm64.tar.gz"
+LINUX_TAR_URL_ARM="https://github.com/dvdmuckle/spc/releases/download/${VERSION}/spc-${VERSION}-linux-arm64.tar.gz"
 
-CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${MAC_TAR_URL}`
+
+CHECKVER_CODE=`curl -X HEAD -m 3 -sfw "%{response_code}" ${MAC_TAR_URL_X64}`
 if [ $CHECKVER_CODE -ne 302 ]; then
 	echo "Version ${VERSION} does not exist" >&2
 	exit 3
 fi
 
-echo "Fetching Linux sha256"
+echo "Fetching Linux X64 sha256"
 #LINUX_SHA=$(curl -sLS "${LINUX_SHA_URL}" | cut -f1 -d\ "")
-LINUX_SHA=$(curl -sLS "${LINUX_TAR_URL}" | shasum -a 256 | cut -f1 -d\ "")
-echo "Fetching macOS sha256"
+LINUX_SHA_X64=$(curl -sLS "${LINUX_TAR_URL_X64}" | shasum -a 256 | cut -f1 -d\ "")
+echo "Fetching macOS X64 sha256"
 #MAC_SHA=$(curl -sLS "${MAC_SHA_URL}" | cut -f1 -d\ "")
-MAC_SHA=$(curl -sLS "${MAC_TAR_URL}" | shasum -a 256 | cut -f1 -d\ "")
+MAC_SHA_X64=$(curl -sLS "${MAC_TAR_URL_X64}" | shasum -a 256 | cut -f1 -d\ "")
+echo "Fetching Linux ARM sha256"
+#LINUX_SHA=$(curl -sLS "${LINUX_SHA_URL}" | cut -f1 -d\ "")
+LINUX_SHA_ARM=$(curl -sLS "${LINUX_TAR_URL_ARM}" | shasum -a 256 | cut -f1 -d\ "")
+echo "Fetching macOS ARM sha256"
+#MAC_SHA=$(curl -sLS "${MAC_SHA_URL}" | cut -f1 -d\ "")
+MAC_SHA_ARM=$(curl -sLS "${MAC_TAR_URL_ARM}" | shasum -a 256 | cut -f1 -d\ "")
 
 cat > Formula/spc.rb <<FORMULA
 class Spc < Formula
@@ -37,13 +46,24 @@ class Spc < Formula
   desc "A spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
 
-  if OS.mac?
-    url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
-    sha256 "${MAC_SHA}"
-  elsif OS.linux?
-    url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
-    sha256 "${LINUX_SHA}"
-end
+  if Hardware::CPU.intel?
+    if OS.mac?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
+      sha256 "${MAC_SHA_X64}"
+    elsif OS.linux?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
+      sha256 "${LINUX_SHA_X64}"
+    end
+  end
+  if Hardware::CPU.arm?
+    if OS.mac?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-arm64.tar.gz"
+      sha256 "${MAC_SHA_ARM}"
+    elsif OS.linux?
+      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-arm64.tar.gz"
+      sha256 "${LINUX_SHA_ARM}"
+    end
+  end
 
   def install
     bin.install "spc"
