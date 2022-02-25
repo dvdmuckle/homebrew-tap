63cf7d8104be56710bce664c24af1bddbe1cbe5c
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-02-25 15:20:19 -0800

    This is silly

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -1,8 +1,14 @@
 class Spc < Formula
   desc "Spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
-  url "https://github.com/dvdmuckle/spc/archive/#{version}.tar.gz"
+  url "https://github.com/dvdmuckle/spc/archive/1.1.1.tar.gz"
   version "1.1.1"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   bottle do
     root_url "https://ghcr.io/v2/dvdmuckle/tap"
   end
