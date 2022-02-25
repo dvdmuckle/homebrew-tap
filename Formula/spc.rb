88ba142e9963321813a4991916503f0c1b37e077
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-02-25 15:27:23 -0800

    Add sha

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -2,6 +2,7 @@ class Spc < Formula
   desc "Spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
   url "https://github.com/dvdmuckle/spc/archive/1.1.1.tar.gz"
+  sha256 "544f38874be3a902b0b5ab38593c3802acd469acad103ecb220d03eef6652f55"
   version "1.1.1"
 
   livecheck do
