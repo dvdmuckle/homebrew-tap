52186eca832154de0f6b9b0f44c102288ac218d0
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-02-25 14:47:48 -0800

    Test automated formula creation

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -2,27 +2,11 @@ class Spc < Formula
   version "1.1.1"
   desc "A spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
-
-  if Hardware::CPU.intel?
-    if OS.mac?
-      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-amd64.tar.gz"
-      sha256 "58ae3964ca15acb67162562ca65c8b86f06344457bcaeb076cbe27ae565851a6"
-    elsif OS.linux?
-      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-amd64.tar.gz"
-      sha256 "427b08adb22ca8fb46a54d7df599e1f558a5f4e6c9493a2bfb75a5a091e672f2"
-    end
-  end
-  if Hardware::CPU.arm?
-    if OS.mac?
-      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-darwin-arm64.tar.gz"
-      sha256 "14eb8680efab7a907de0412ae0bfa6e62df20dd67c658d635256908d94134278"
-    elsif OS.linux?
-      url "https://github.com/dvdmuckle/spc/releases/download/#{version}/spc-#{version}-linux-arm64.tar.gz"
-      sha256 "c3ea843d3ca5a55f710ecd3aec4670053545b295c9cb5585456c51810ddb423b"
-    end
-  end
+  url "https://github.com/dvdmuckle/spc/archive/#{version}.tar.gz"
+  depends_on "go" => :build
 
   def install
+    system "go", "build", "-o", "spc", "-ldflags", "-X github.com/dvdmuckle/spc/cmd.version=#{version}"
     bin.install "spc"
 
     output = Utils.safe_popen_read({ "SHELL" => "bash" }, bin/"spc", "completion", "bash")
