4cb382c9fc8659afa0579197958613358652cb0e
David Muckle <dvdmuckle@dvdmuckle.xyz>
2022-03-05 01:43:31 -0500

    Update to 1.1.2

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -1,7 +1,7 @@
 class Spc < Formula
   desc "Lightweight Spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
-  url "https://github.com/dvdmuckle/spc/archive/1.1.1.tar.gz"
+  url "https://github.com/dvdmuckle/spc/archive/1.1.2.tar.gz"
   sha256 "544f38874be3a902b0b5ab38593c3802acd469acad103ecb220d03eef6652f55"
 
   livecheck do
