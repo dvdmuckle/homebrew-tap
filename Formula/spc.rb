35f83d47e8562e612a7211549f308584e60ebc46
David Muckle <dvdmuckle@dvdmuckle.xyz>
2025-06-24 17:49:45 -0400

    spc 1.3.3
    Created by https://github.com/mislav/bump-homebrew-formula-action
    
    Closes #19.
    
    Signed-off-by: David Muckle <dvdmuckle@dvdmuckle.xyz>

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -1,8 +1,8 @@
 class Spc < Formula
   desc "Lightweight Spotify CLI"
   homepage "https://github.com/dvdmuckle/spc"
-  url "https://github.com/dvdmuckle/spc/archive/refs/tags/1.3.2.tar.gz"
-  sha256 "dbf3871b4ad01413822c28f464ec1ab80acad72e02983fc9e80d96db9e5c927b"
+  url "https://github.com/dvdmuckle/spc/archive/refs/tags/1.3.3.tar.gz"
+  sha256 "fef3419842af48d4989ccddeafaca8a2178b55f142beb2bf07fb9e6f47c0418a"
 
   livecheck do
     url :stable
