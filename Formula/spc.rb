a79efd80224f723b6680bd2b4211bfc30b4e4646
David Muckle <dvdmuckle@dvdmuckle.xyz>
2026-07-18 19:09:48 +0000

    spc: update 1.3.5 bottle.

--- Formula/spc.rb
+++ Formula/spc.rb
@@ -11,10 +11,10 @@ class Spc < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/dvdmuckle/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f81f66039ec970841b94047427937a97a3b420367b1fd8226be77e06d4ce0a25"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6af6fc6f341b94987ef9e2e45acce2c7e6be3fbbe71a40d25a5c38be22012186"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5a86b4227d3d963cfc99008607ad6723ae591f35188a78b554544f9c23269573"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ee6350877b90ddb6bdf4f8e8b96bcd4923f86861b5908965a1f794bf3a26f42"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b691a8b500c300be6030bf734fa3ab8b813991fcb2b9f82afc344547c785331e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "57e5ba5dd6af9c59d8b9cf4b32e220335b9c60ee434f8033642d976eda2f3024"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5cdacb332f64e511c4f7d1db5187ba7276110321d96fb82faeb19b4cc8e4342d"
+    sha256 cellar: :any,                 x86_64_linux:  "266626c1501de6900685b27847c4e088d0fd334484a57b20ebede50dc6a402e9"
   end
   depends_on "go" => :build
 
